bbf18900aeadaf23009bcd23ced1e71f5fa7742a
Elai5 <yvettelaine007@gmail.com>
2024-09-28 17:12:49 +0300

    functions1

---
+++ code/fundamentals2.js
@@ -0,0 +1,15 @@
+// lec1: functions
+let country;
+let population;
+let capitalCity;
+
+function describeCountry(country, population, capitalCity) {
+    // return ($(country + 'has ' + ${population})
+    return (`${country} has ${population} million people and its capital city is ${capitalCity}`);
+
+};
+
+const itsKenya = describeCountry('kenya', 20, 'Nairobi')
+describeCountry();
+describeCountry();
+
