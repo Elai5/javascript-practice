a8012113b43ac598c7c5a2eadad4d83c814e3353
Elai5 <yvettelaine007@gmail.com>
2024-09-28 10:06:04 +0300

    declaring scope

---
+++ code/scope.js
@@ -0,0 +1,8 @@
+const city = "New York City";
+function logCitySkyline() {
+    console.log(logCitySkyline());
+    let skyscraper = "Empire State Building";
+    return "The stars over the " + skyscraper + " in " + city;
+}
+/*You’ll notice that the logCitySkyline() function is able to access both variables without any problems.
+ In the next exercise we’ll consider why would it be preferable to have one variable outside of a block and the other inside of a block. */
