f69f818f860996622b8a859803f20f915420ff83
Elai5 <yvettelaine007@gmail.com>
2024-09-28 10:29:03 +0300

    global variable

--- code/scope.js
+++ code/scope.js
@@ -6,3 +6,14 @@ function logCitySkyline() {
 }
 /*You’ll notice that the logCitySkyline() function is able to access both variables without any problems.
  In the next exercise we’ll consider why would it be preferable to have one variable outside of a block and the other inside of a block. */
+
+ //global variable
+ const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+function callMyNightSky(){
+  console.log(callMyNightSky());
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+ 
+};
