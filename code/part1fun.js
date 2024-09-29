9280b74e70aa09c445d9f975bba3e01e3e708232
Elai5 <yvettelaine007@gmail.com>
2024-09-29 22:15:51 +0300

    new file. dealing with operations

---
+++ code/part1fun.js
@@ -0,0 +1,10 @@
+let johnMass = 78;
+let johnHeight = 1.69;
+
+let markMass = 95;
+let markHeigth = 1.88;
+
+let BMI = (johnMass / (johnHeight ** 2));
+let BMI2 = (markMass / (markHeigth ** 2));
+console.log(BMI);
+console.log(BMI2)
