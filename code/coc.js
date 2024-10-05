3cba5ccbe9593ff1e17ef9a3966903c1254d16c3
Elai5 <yvettelaine007@gmail.com>
2024-10-05 22:19:33 +0300

    precedence in operators

--- code/coc.js
+++ code/coc.js
@@ -1,11 +1,21 @@
-const plantNeedsWater = function(day) {
+const plantNeedsWater = function (day) {
     if (day === 'wednesday') {
         return true;
     } else {
         return false;
     }
 }
-day = plantNeedsWater( 'wednesday')
+day = plantNeedsWater('wednesday')
 console.log(day);
 
-// const plantsNeedWater = day => day === 'wednesday' ? true : false;
+// const plantsNeedWater = day => day === 'wedesday' ? true : false;
+
+let age = 14;
+const isHoliday = false;
+
+// if (isHoliday &&(age <= 6 || age >= 65)) {}
+if (age <= 6 || (age >= 65 && isHoliday)) {
+    console.log('Discount');
+} else {
+    console.log('No Discount');
+}
