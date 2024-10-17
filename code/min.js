408fc688051079a35b7a4b4eadfdbbbd4c2c1395
Elai5 <yvettelaine007@gmail.com>
2024-10-17 12:11:42 +0300

    use reduce() to get min number in an array

--- code/min.js
+++ code/min.js
@@ -1,6 +1,32 @@
 let shark = [120, 130, 105, 100, 115];
-let dolphin = [125, 90, 100, 120, 95]
+let dolphin = [125, 90, 100, 120, 95];
 
-let calcAverage = (match1, match2, match3, mathc4, match5) => {
-    return(match1)
-}
+let smallestScoreShark = Math.min(...shark);
+let smallestScoreDolphin = Math.min(...dolphin);
+
+
+shark =
+    shark.reduce((newShark, currentElement) => {
+        if (currentElement !== smallestScoreShark) {
+            newShark.push(currentElement);
+        }
+        return newShark;
+    }, [])
+console.log(shark)
+dolphin =
+    dolphin.reduce((newDolphin, currentElement) => {
+        if (currentElement !== smallestScoreDolphin) {
+            newDolphin.push(currentElement);
+        }
+        return newDolphin;
+    }, [])
+console.log(dolphin)
+
+
+let calcAverage = (...scores) => {
+    return scores.reduce((sum, score) => sum + score, 0) / scores.length;
+};
+let sharkAvg = calcAverage(...shark)
+console.log(sharkAvg);
+let dolphinAvg = calcAverage(...dolphin)
+console.log(dolphinAvg);
