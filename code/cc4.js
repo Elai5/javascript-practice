cfd8c4b2166d972b0e5b9c7602bb52099be0e79b
Elai5 <yvettelaine007@gmail.com>
2024-10-17 12:31:57 +0300

    new comment explaining code  on min.js and rtyout on cc4.js to test new skill

--- code/cc4.js
+++ code/cc4.js
@@ -17,3 +17,34 @@ bill >= 50 && bill <= 300
 // }
 
 // console.log(tip);
+
+
+let team1 = [44, 23, 71, 56];
+let team2 = [65, 54, 49, 103];
+
+let smallestScoreTeam1 = Math.min(...team1);
+let smallestScoreTeam2 = Math.min(...team2);
+
+team1 =
+    team1.reduce((newTeam1, currentElement) => {
+        if (currentElement !== smallestScoreTeam1) {
+            newTeam1.push(currentElement);
+        }
+        return newTeam1
+    }, [])
+console.log(team1)
+
+team2 =
+    team2.reduce((newTeam2, currentElement) => {
+        if (currentElement !== smallestScoreTeam2) {
+            newTeam2.push(currentElement);
+        }
+        return newTeam2;
+    }, [])
+console.log(team2);
+
+calcAverage = (...values) => {
+    return values.reduce((sum, values) => sum + values, 0) / values.length;
+};
+let team1Avg = calcAverage(...team1)
+console.log(team1Avg)
--- code/min.js
+++ code/min.js
@@ -1,10 +1,15 @@
+//we start by adding two arrays for the two teams with diffrent values.
+
 let shark = [120, 130, 105, 100, 115];
 let dolphin = [125, 90, 100, 120, 95];
-
+// we then use math function the min to sample out the smallest value in the list of array
+// we create two so that we dont run into errors, and treat ech component as an individual.
 let smallestScoreShark = Math.min(...shark);
 let smallestScoreDolphin = Math.min(...dolphin);
-
-
+//we have used the rest parameter(...shark) to insicate that we ae accesing the elements inside the array
+// as an individual
+// The spread operator (...shark) is used here to "spread" the elements of the shark array
+// into individual arguments for the Math.min function.
 shark =
     shark.reduce((newShark, currentElement) => {
         if (currentElement !== smallestScoreShark) {
@@ -13,6 +18,19 @@ shark =
         return newShark;
     }, [])
 console.log(shark)
+// we start by reinitializing shark. we use the reduce funcion to iterate through the Array
+// we add a n accumlator with the name newSHark, and current Element as our parameters, using the arrow function
+// we proceed to use a conditional statement that checks if currentElement is not the smallestScore Iin shark
+// we pass the argument, we push all the elements in the aray to new shark. initialy it is set to 
+// an empty array so that it goes over the shark and stores the first valu,e then iterates to the secong and so on till it finds the smalles and reaches the end.
+//  after which we use return since its a function and later on log log shark
+
+// Check if the current element is not the smallest score.
+// If not, add the current element to the newShark array.
+// The reduce function iterates through the array. 
+// 'newShark' is the accumulator that starts as an empty array, and 'currentElement' is each element being processed.
+// If the current element is not the smallest score, it's pushed into the newShark array.
+// The result is a new array without the smallest score.
 dolphin =
     dolphin.reduce((newDolphin, currentElement) => {
         if (currentElement !== smallestScoreDolphin) {
@@ -21,12 +39,23 @@ dolphin =
         return newDolphin;
     }, [])
 console.log(dolphin)
+// /we do the same for dolphin
+// when calculatin the average, we use now the spread parameter, as we are passing the values as an individual
+// using arrow function, our two parameters being sum and scores. we use return
+// scores.reduce as reduce iterates through the array, pass in arg sum + score a , and 0 as the initial value of sum is set to 0.
+//  we then later divide by scores.length as it checks num of items in the scores and divides by it.Math
 
 
 let calcAverage = (...scores) => {
     return scores.reduce((sum, score) => sum + score, 0) / scores.length;
 };
+// To calculate the average, we use a rest parameter (...scores) to accept any number of scores.
+// The arrow function takes 'scores' (an array of values), sums them using reduce, and divides by the length.
+
+
 let sharkAvg = calcAverage(...shark)
 console.log(sharkAvg);
 let dolphinAvg = calcAverage(...dolphin)
 console.log(dolphinAvg);
+// When calculating the individual averages, the spread operator is used to pass each array's elements
+// as separate arguments to the calcAverage function.
