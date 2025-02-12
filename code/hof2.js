6763b061af1e1dabc6565f9e92379c95e84f3ccf
Elai5 <yvettelaine007@gmail.com>
2025-02-12 17:37:33 +0300

    highrer order call back functions implemented

--- code/hof2.js
+++ code/hof2.js
@@ -9,3 +9,18 @@ console.log(addItUp);
 
 const sum = (x, y) => x + y;
 sum.alias = 'summation';
+
+let multiplyTwoValues = (val1, val2) => {
+    return val1 * val2;
+}
+
+let square = (val) => {
+    return val * val;
+}
+
+let getFUnctionString = (func) => {
+    return func.toString()
+}
+
+let result = multiplyTwoValues(2, 2);
+square(result);
