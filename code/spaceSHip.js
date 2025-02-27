5a18ed82d034ea3f89a9285b1b15d91dad32a53a
Elai5 <yvettelaine007@gmail.com>
2025-02-27 11:51:58 +0300

    nested object, accesing array with index and chain properties

---
+++ code/spaceSHip.js
@@ -0,0 +1,32 @@
+let spaceShip = {
+  passenger: null,
+  telescope: {
+    yearBuil: 2018,
+    model: "91031-XLT",
+    focalength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      favoriteFoods: ["cookies", "cakes", "carrot"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+let capFave = spaceShip.crew.captain["favoriteFoods"][0];
