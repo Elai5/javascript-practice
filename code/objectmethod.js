bf39639b82c4d69dc405bec894df016711b3afd7
Elai5 <yvettelaine007@gmail.com>
2025-02-27 11:30:23 +0300

    explanation for object methods

--- code/objectmethod.js
+++ code/objectmethod.js
@@ -1,3 +1,8 @@
+// in this practice, we learn about object methods in js.
+//objects method are invoked by appending objects with name. 
+// in this istance, we have the retreat message, we create object alienship with method retreat
+// and takeoff, bth methods are separated by comma,and then invoked at the end. 
+
 let retreatMessage =
   "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
 
