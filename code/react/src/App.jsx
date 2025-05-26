5f970932c1bfc44add1c2df04e8d71b37a55d425
Elai5 <yvettelaine007@gmail.com>
2025-05-27 01:25:08 +0300

    initialized react app for usehook practice

---
+++ code/react/src/App.jsx
@@ -0,0 +1,21 @@
+/** @format */
+
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-white text-white">
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
