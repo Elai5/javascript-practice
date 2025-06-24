7af68ee6340964a1ae57dbbae18c780fd789a380
Elai5 <yvettelaine007@gmail.com>
2025-06-25 00:08:42 +0300

    added two sum challenge from leetcode

---
+++ leetcode/strings.js
@@ -0,0 +1,20 @@
+/** @format */
+
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+// If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    
+};
+
+// what if we use nested loop,  first off, i would check which word is the longest, then, 
+// identify lenght of both words
+// initiate i, check if i is great than length of word 1,then add the first letter then run the inner loop j, add the index 0, thne go back to the firt loop add the second
+//input size increases,run time increases.
